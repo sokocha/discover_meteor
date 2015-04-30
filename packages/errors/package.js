cc0524e005386521e7159d6c7183db7660b1b2c6
Sadiq Okocha <sokocha@gmail.com>
2015-04-30 17:56:47 +0100

    commit 9-5-2

--- packages/errors/package.js
+++ packages/errors/package.js
@@ -1,21 +1,22 @@
 Package.describe({
-  summary: "A pater to display application errors to the user"
+  summary: "A pattern to display application errors to the user",
+  version: "1.0.0"
 });
 
-Package.on_use(function (api, where) {
+Package.onUse(function (api, where) {
+  api.versionsFrom('0.9.0');
+  
   api.use(['minimongo', 'mongo-livedata', 'templating'], 'client');
-  api.add_files(['errors.js', 'errors_list.html', 'errors_list.js'], 'client');
 
-  if (api.export) {
+  api.addFiles(['errors.js', 'errors_list.html', 'errors_list.js'], 'client');
+  
+  if (api.export) 
     api.export('Errors');
-  }
 });
 
-
-
-
-
-
-
-
-
+Package.onTest(function(api) {
+  api.use('errors', 'client');
+  api.use(['tinytest', 'test-helpers'], 'client');  
+  
+  api.addFiles('errors_tests.js', 'client');
+});
